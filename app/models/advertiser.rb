113a062a7d41de373920bab2fb8b52b7e3c32b01
ntholi <ntholi@live.com>
2016-11-06 07:39:36 +0200

    Allow post to be made in adverts

--- app/models/advertiser.rb
+++ app/models/advertiser.rb
@@ -1,3 +1,18 @@
 class Advertiser < ApplicationRecord
 	has_many :adverts
+
+	def fullname
+		"#{first_name} #{second_name}"
+	end
+	def name
+		if company_name
+			return company_name
+		else
+			return fullname
+		end
+	end
+
+	def to_s
+		return name
+	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :advertiser
+
+  def to_s
+  	"#{name} at #{price}"
+  end
 end
