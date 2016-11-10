54c0bb28a86ad54c0df93cdad132f41ccf3ba12f
ntholi <ntholi@live.com>
2016-11-10 14:35:35 +0200

    Update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,8 +16,11 @@ class PostsController < ApplicationController
   def new
     @post = Post.new;
     # if(params.has_key?(:min_price)) && params.has_key
-    min_price = params['min_price']
-    @post.min_price = min_price
+    max_price = params['max_price']
+    content = params['content']
+
+    @post.max_price = max_price
+    @post.content = content
   end
 
   # GET /posts/1/edit
@@ -27,7 +30,9 @@ class PostsController < ApplicationController
   def pre_create
     content = params[:post][:content]
     price = get_price(content)
-    redirect_to controller: 'posts', action: 'new', min_price: price
+    category = get_category(content)
+
+    redirect_to controller: 'posts', action: 'new', content: content, max_price: price
   end
 
   # POST /posts
@@ -72,6 +77,9 @@ class PostsController < ApplicationController
 
   def get_price(content)
     price = content[ /\d+(?:\.\d+)?/ ]
+  end
+
+  def get_category(content)
   end
 
   private
