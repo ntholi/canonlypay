b35a5310ca9ac040cd16589116ec2c3a17792659
ntholi <ntholi@live.com>
2017-02-23 16:20:32 +0200

    Fix home routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,8 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'home/index'
-  
+  get 'home/index'
+
   root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
