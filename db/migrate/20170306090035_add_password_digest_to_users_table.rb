5d562a56d01010e2d21f2b7d0e46349f360fc44c
ntholi <ntholi@live.com>
2017-03-09 12:35:36 +0200

    Make password required

--- db/migrate/20170306090035_add_password_digest_to_users_table.rb
+++ db/migrate/20170306090035_add_password_digest_to_users_table.rb
@@ -1,5 +1,5 @@
 class AddPasswordDigestToUsersTable < ActiveRecord::Migration[5.0]
   def change
-    add_column("users", 'password_digest', :string)
+    add_column("users", 'password_digest', :string, required: true)
   end
 end
