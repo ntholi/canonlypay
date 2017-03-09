54f5103a2ef889c5ad34efe80cc3969214f90681
ntholi <ntholi@live.com>
2017-03-09 12:49:31 +0200

    Add has_secure_password on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ApplicationRecord
   has_many :posts
+
+  has_secure_password
+  
   validates :phone_number, presence: true
   validates_uniqueness_of :phone_number
 
   GENDER_TYPES = ["","Male", "Female", "Other"]
 
   def full_name
-  	"#{first_name last_name}"	
+  	"#{first_name last_name}"
   end
 
   def to_s
