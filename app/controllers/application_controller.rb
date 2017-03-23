2196748c9ce7a8c194bfe6db8a8cd42c7f73186c
ntholi <ntholi@live.com>
2017-03-23 13:47:11 +0200

    Use a different layout for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :prepere_data_for_loggedout_users
+
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
+
+  def prepere_data_for_loggedout_users
+    @adverts = Advert.all
+  end
 end
