e4333cde0df15cfa814d71db0cfbf46dda9bda14
ntholi <ntholi@live.com>
2017-03-24 14:50:58 +0200

    accepts_nested_attributes_for :product

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -2,6 +2,6 @@ class Advert < ApplicationRecord
   belongs_to :company
   belongs_to :product
   # accepts_nested_attributes_for :company
-  # accepts_nested_attributes_for :product
+  accepts_nested_attributes_for :product
   has_many :advert_comments
 end
