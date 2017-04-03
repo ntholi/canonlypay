3a516d446cf7a32da8abe4ad73ad005a0db7a031
ntholi <ntholi@live.com>
2017-04-03 10:29:23 +0200

    rails g happy_seed:devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324115846) do
+ActiveRecord::Schema.define(version: 20170403081945) do
 
   create_table "advert_comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -103,10 +103,6 @@ ActiveRecord::Schema.define(version: 20170324115846) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
