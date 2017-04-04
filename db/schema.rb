d4b1baccfe75706aecbc0480635ef978b8eff960
ntholi <ntholi@live.com>
2017-04-04 14:14:45 +0200

    Add display name and add avatar.png

--- db/schema.rb
+++ db/schema.rb
@@ -110,7 +110,6 @@ ActiveRecord::Schema.define(version: 20170403093717) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "display_name"
     t.string   "first_name"
     t.string   "last_name"
     t.string   "gender"
