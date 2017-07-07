b2af0babe4214f29b57825b2c3c6c125b4b19833
ntholi <ntholi@live.com>
2017-07-07 15:39:47 +0200

    Filter is now working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
-    if(params[:city])
+    if params[:city] && params[:city] != 'all'
       @posts = Post.where(city: params[:city]).order('created_at DESC')
+    elsif params[:city] == 'all'
+      @posts = Post.all.order('created_at DESC')
     else
       @posts = Post.all.order('created_at DESC')
     end
