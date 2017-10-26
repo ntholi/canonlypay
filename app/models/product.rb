909b387333e25111a78f4b5d68ecd9388e41a5b4
Ntholi Nkhatho <ntholi@live.com>
2017-10-26 15:05:55 +0200

    product name is not optional

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,14 @@ class Product < ApplicationRecord
   belongs_to :product_category
   has_many :adverts
   has_many :posts
+  validates_presence_of :name;
   # belongs_to :company, required: false
 
   def summary
-  	"#{name.indefinite_article.capitalize} #{name} for R#{price}"
+    short_product_description = ""
+    if !name.blank?
+      short_product_description = "#{name.indefinite_article.capitalize} #{name}"
+    end
+  	"#{short_product_description} for R#{price}"
   end
 end
